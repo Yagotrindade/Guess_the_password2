ff2211b82e8e90597805ea0e8e949a8cb7fa9b8a
gabriel <yagogabriel850@gmail.com>
2025-04-05 08:12:55 -0300

    update message

--- resources/js/script.js
+++ resources/js/script.js
@@ -11,7 +11,7 @@ const mensagens = [
         "- A chave está na afirmação 1."
     ],
     [
-        "- O texto cifrado é: Ft qfit ij ktwf it utwyfq vzj htsjhyf tx itnx rzsitx, wjxnij t ufxxj ufwf t uwócnrt ijxfknt.",
+        "- O texto cifrado é: jsywj tx ywfutx, à inwjnyf ij zrf hmfrf rtwyf, ijxhfsxf f hmfaj if vzfwyf wtxf.",
         "- A chave desta afirmação está correta.",
         "- A chave é 5."
     ]
@@ -37,6 +37,7 @@ function verificarCifra() {
     let texto = document.getElementById("inputTexto").value;
     let chave = parseInt(document.getElementById("inputChave").value);
     let resultadoDiv = document.getElementById("resultado");
+    let mensagemDiv = document.getElementById("mensagem");
 
     if (!texto || isNaN(chave) || chave < 1 || chave > 25) {
         resultadoDiv.innerText = "Por favor, insira um texto e uma chave válida (1-25).";
@@ -52,8 +53,8 @@ function verificarCifra() {
     }).join('');
 
     resultadoDiv.innerText = `Texto decifrado: ${decifrado}`;
-    if (decifrado === "Ao lado de fora do portal que conecta os dois mundos, reside o passe para o próximo desafio." || decifrado === "Ao lado de fora do portal que conecta os dois mundos, reside o passe para o próximo desafio") {
-        resultadoDiv.innerText += "\nParabéns! Você decifrou o texto corretamente!";
+    if (decifrado === "entre os trapos, à direita de uma chama morta, descansa a chave da quarta rosa." || decifrado === "entre os trapos, à direita de uma chama morta, descansa a chave da quarta rosa." || decifrado === " entre os trapos, à direita de uma chama morta, descansa a chave da quarta rosa." || decifrado === " entre os trapos, à direita de uma chama morta, descansa a chave da quarta rosa") {
+        mensagemDiv.innerText += "\n...A princesa pega a chave em meio aos trapos, e abre a porta que a guardava. O último desafio estava próximo, ela pega a rosa e vai para o sul da parte mais alta da torre...";
         trocar("trocar1", "./resources/images/tulipaComplete.png", 400);
         trocar("trocar2", "./resources/images/tulipaComplete.png", 600);
         trocar("trocar3", "./resources/images/tulipaComplete.png", 800);
